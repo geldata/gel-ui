20f6e6072a0915de6d7a26a8891483eaadc12195
James Clarke <james@edgedb.com>
2025-09-17 21:03:42 +0100

    Add updated theme switcher design

---
+++ shared/common/newui/themeSwitcher/index.tsx
@@ -0,0 +1,39 @@
+import {Theme, useTheme} from "../../hooks/useTheme";
+import cn from "../../utils/classNames";
+
+import {DarkThemeIcon, LightThemeIcon, SystemThemeIcon} from "./icons";
+
+import styles from "./themeSwitcher.module.scss";
+
+const themes = [Theme.light, Theme.system, Theme.dark];
+
+const themeIcons = {
+  [Theme.light]: <LightThemeIcon />,
+  [Theme.system]: <SystemThemeIcon />,
+  [Theme.dark]: <DarkThemeIcon />,
+};
+
+export function ThemeSwitcher() {
+  const [theme, _, setTheme] = useTheme();
+
+  return (
+    <div className={styles.themeSwitcher}>
+      <div
+        className={styles.selectedMarker}
+        style={{transform: `translateX(${themes.indexOf(theme) * 30}px)`}}
+      />
+
+      {themes.map((t) => (
+        <div
+          key={t}
+          className={cn(styles.option, {
+            [styles.selected]: theme === t,
+          })}
+          onClick={() => setTheme(t)}
+        >
+          {themeIcons[t]}
+        </div>
+      ))}
+    </div>
+  );
+}
